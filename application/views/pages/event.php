c2a7477bd4e2014fc73229122238a7d6868e977c
tomjar <tomreese86@gmail.com>
2013-11-03 11:52:37 -0600

    added event.php mockup

---
+++ application/views/pages/event.php
@@ -0,0 +1,44 @@
+<!doctype html>
+
+<html>
+  
+  <head>
+    <title>New Page</title>
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="https://djyhxgczejc94.cloudfront.net/frameworks/bootstrap/3.0.0/themes/white-plum/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js" type="text/javascript"></script>
+    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.0.1/js/bootstrap.min.js" type="text/javascript"></script>
+  </head>
+  
+  <body>
+    <h2>New Event</h2>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <form>
+            <label class="form-label">Name</label>
+            <input class="form-control" type="text">
+            <label class="form-label">Location</label>
+            <input class="form-control" type="text">
+            <label class="form-label">Datetime</label>
+            <input class="form-control" type="text">
+            <label class="form-label">Description</label>
+            <textarea class="form-control"></textarea>
+            <label class="form-label">Category</label>
+            <select class="form-control">
+              <option>Option 1</option>
+              <option>Option 2</option>
+              <option>Option 3</option>
+            </select>
+          </form>
+        </div>
+        <div class="col-md-6">
+          Helpful info over here, such as prompts, hmm not sure, let me know what you think.
+          When the page is first visited their will be a url query called "mode", mode could be edit, insert. As well as the "primary key" of the item if in edit mdoe. Make sense?
+          
+        </div>
+      </div>
+    </div>
+  </body>
+
+</html>
