833a6f14ba08f5e0b3689393c5192f781d5a4267
Sammons <bsammnz@gmail.com>
2013-11-05 00:22:22 -0600

    both modals pretty, now to put them everywhere

--- Ben/common.js
+++ Ben/common.js
@@ -11,14 +11,29 @@ $(document).ready(function() {
 
 	$(".login").click(function(e) {
 		//trigger login modal
-		$("#login").modal();
+		$("#login").modal('toggle');
+	});
+	$(".login-submit").click(function(e) {
+		//check if valid inputs (later)
+
+		//send object of login data via ajax
+
+		//if success dismiss modal, and redirect
+		$("#login").modal('toggle');
 	});
 
 	$(".register").click(function(e) {
 		//trigger register modal
-		$("#register").modal();
+		$("#register").modal('toggle');
 	});
+	$(".register-submit").click(function(e) {
+		//check if valid inputs (later)
 
+		//send object of register data via ajax
+
+		//if success dismiss modal, and redirect
+		$("#register").modal('toggle');
+	});
 	$(".search").keypress(function(event) {
 		//should be used to capture enter key for searchbars
 		if (event.which=="13") {
