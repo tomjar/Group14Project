c8472f446b0d6acaa953e9318ddd6c500746ca8b
Sammons <bsammnz@gmail.com>
2013-11-05 02:08:20 -0600

    frontend prototypage is close enough for me, time to start setting up db a bit, targeting auth first

--- Ben/common.js
+++ Ben/common.js
@@ -3,43 +3,57 @@ $(document).ready(function() {
 
 
 
-	$(".block-default").click(function(e) {
-		//stops page refresh for elements that have this class
-		return false;
-	})
+    $(".block-default").click(function(e) {
+        //stops page refresh for elements that have this class
+        return false;
+    });
+    var b = true;
+    $(".toggle-edit").click(function(e) {
+    	b = !b;
+    	//trigger editability of fields on page
+    	//note that this means that when the server recieves a request 
+    	//the validity of the session MUST be checked
+    	if (!b) {
+    	$(".toggle-edit").text("save");
+    	$(".editable").attr("contenteditable","true");
+    	} else {
+    	$(".toggle-edit").text("edit");
+    	//scrape data and ajax it off
+    	$(".editable").attr("contenteditable","false");
+    	}
+    	return false;
+    });
+    $(".login").click(function(e) {
+        //trigger login modal
+        $("#login").modal('toggle');
+    });
+    $(".submit-login").click(function(e) {
+        //check if valid inputs (later)
+        console.log("login subumit")
+        //send object of login data via ajax
 
+        //if success dismiss modal, and redirect
+        $("#login").modal('toggle');
+    });
 
-	$(".login").click(function(e) {
-		//trigger login modal
-		$("#login").modal('toggle');
-	});
-	$(".login-submit").click(function(e) {
-		//check if valid inputs (later)
+    $(".register").click(function(e) {
+        //trigger register modal
+        $("#register").modal('toggle');
+    });
+    $(".submit-register").click(function(e) {
+        //check if valid inputs (later)
 
-		//send object of login data via ajax
+        //send object of register data via ajax
 
-		//if success dismiss modal, and redirect
-		$("#login").modal('toggle');
-	});
-
-	$(".register").click(function(e) {
-		//trigger register modal
-		$("#register").modal('toggle');
-	});
-	$(".register-submit").click(function(e) {
-		//check if valid inputs (later)
-
-		//send object of register data via ajax
-
-		//if success dismiss modal, and redirect
-		$("#register").modal('toggle');
-	});
-	$(".search").keypress(function(event) {
-		//should be used to capture enter key for searchbars
-		if (event.which=="13") {
-		window.location.href =window.location.origin+"/listing.html?search="+$(this).val();
-		}
-		console.log(this.value);
-	});
+        //if success dismiss modal, and redirect
+        $("#register").modal('toggle');
+    });
+    $(".search").keypress(function(event) {
+        //should be used to capture enter key for searchbars
+        if (event.which == "13") {
+            window.location.href = window.location.origin + "/listing.html?search=" + $(this).val();
+        }
+        console.log(event.which);
+    });
 
 });
